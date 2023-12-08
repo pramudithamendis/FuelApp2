079d991f1a45b95fc5c3428e2238c90666dbd2d9
Pramuditha <pramudithamendis444@gmail.com>
2023-12-08 16:11:53 +0530

    Added the content inside the updateFilledIn()

--- src/main/java/fuel/Fuel.java
+++ src/main/java/fuel/Fuel.java
@@ -28,8 +28,30 @@ public class Fuel {
         }
 	}
 	
-	public void updateFilledIn(int n) {
-		
+	public void updateFilledIn(String vReg, int licenseNo, int amount) {
+		System.out.println("starting");
+
+        String host = "localhost";
+        String instance = "DESKTOP-H0GHTEU";
+        String database = "jdbc";
+        try{
+        String uname = "sa";
+        String pas = "root";
+        String url = "jdbc:sqlserver://"+host+":1433;"+"instance="+instance+":databaseName="+database+";encrypt=true;trustServerCertificate=true";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+//        String url = "jdbc:sqlserver://localhost:1433;instance=DESKTOP-H0GHTEU:databaseName=jdbc;encrypt=true;trustServerCertificate=true";
+//                        System.out.println("2");
+        Connection con = DriverManager.getConnection(url, uname, pas);
+
+        Statement st = con.createStatement();
+//        st.executeUpdate("insert into jdbc.dbo.table1 values(405, 12)");
+        st.executeUpdate("insert into fuel.dbo.FilledIn(vRegNo, licenseNo, amoutn) values('123456', 1, 1)");
+        
+        System.out.println("Inserted to FilledIn table");
+        }catch(Exception e){
+            System.out.println(e);
+        }
 	}
 
 	public static void main(String args[])  throws ClassNotFoundException, SQLException  {
